3c8c943c51db2ccb13a06e28cb76da668cc83efb
Federick <federick.jonathan@traveloka.com>
2023-10-01 12:44:56 +0700

    update varialble naming in tugas1

--- tugas1/121140017_Andreas Pascalis Tristan_Prak1.py
+++ tugas1/121140017_Andreas Pascalis Tristan_Prak1.py
@@ -10,13 +10,13 @@ for enter in range(2):
 
 for x in range(1, 4):
     username = input("Username anda : ")
-    Password = int(input("Password anda : "))
+    password = int(input("Password anda : "))
 
-    if username == "informatika" and Password == 12345678:
+    if username == "informatika" and password == 12345678:
         print("Berhasil login!")
         break
-    elif x == 3 and (username != "informatika" or Password != 12345678):
+    elif x == 3 and (username != "informatika" or password != 12345678):
         print("You've ran out of chances my friend")
-    elif username != "informatika" or Password != 12345678:
+    elif username != "informatika" or password != 12345678:
         print("Username atau password anda salah coba lagi")
 
